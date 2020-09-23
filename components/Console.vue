340057168fe891d4631ca37a77da2249e1454922
rw-simon <simon.fischer@rechtwinklig.ch>
2020-09-23 17:20:17 +0200

    added console, added font support

---
+++ components/Console.vue
@@ -0,0 +1,51 @@
+<template>
+	<div
+		id="console"
+		:class="{ 'bc-blue p-1 br font-mono': true, active: isActive }"
+	>
+		<p>
+			<span @click="toggleConsole" class="clickable toggle-console"
+				>[{{ toggleSymbol }}]</span
+			>
+		</p>
+		<div class="content">
+			<p>Conzepta</p>
+			<p>{Unterstützung gefällig? Nehmen Sie mit uns Kontakt auf.}</p>
+		</div>
+	</div>
+</template>
+
+<script>
+export default {
+	name: 'Console',
+	data() {
+		return {
+			title: '',
+			isActive: true,
+			toggleSymbol: 'X',
+		}
+	},
+	methods: {
+		toggleConsole() {
+			this.isActive = !this.isActive
+			this.toggleSymbol = this.isActive ? 'X' : '<'
+		},
+	},
+}
+</script>
+
+<style lang="sass" scoped>
+#console
+	position: fixed
+	width: 24rem
+	top: 10rem
+	right: -20rem
+	display: grid
+	grid-template-columns: 4rem 1fr
+	transition: right $anim-smooth
+	span.toggle-console
+		&:hover
+			cursor: pointer
+	&.active
+		right: 1rem
+</style>
--- nuxt.config.js
+++ nuxt.config.js
@@ -1,44 +1,60 @@
 export default {
-  // Global page headers (https://go.nuxtjs.dev/config-head)
-  head: {
-    title: 'conzepta',
-    meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' }
-    ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
-  },
+	// Global page headers (https://go.nuxtjs.dev/config-head)
+	head: {
+		title: 'Conzepta',
+		meta: [
+			{ charset: 'utf-8' },
+			{
+				name: 'viewport',
+				content: 'width=device-width, initial-scale=1',
+			},
+			{ hid: 'description', name: 'description', content: '' },
+		],
+		// link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+	},
 
-  // Global CSS (https://go.nuxtjs.dev/config-css)
-  css: [
-  ],
+	// Global CSS (https://go.nuxtjs.dev/config-css)
+	css: ['~/assets/app.sass'],
 
-  // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
-  plugins: [
-  ],
+	// Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
+	plugins: [],
 
-  // Auto import components (https://go.nuxtjs.dev/config-components)
-  components: true,
+	// Auto import components (https://go.nuxtjs.dev/config-components)
+	components: true,
 
-  // Modules for dev and build (recommended) (https://go.nuxtjs.dev/config-modules)
-  buildModules: [
-  ],
+	// Modules for dev and build (recommended) (https://go.nuxtjs.dev/config-modules)
+	buildModules: [],
 
-  // Modules (https://go.nuxtjs.dev/config-modules)
-  modules: [
-    // https://go.nuxtjs.dev/axios
-    '@nuxtjs/axios',
-    // https://go.nuxtjs.dev/pwa
-    '@nuxtjs/pwa',
-  ],
+	// Modules (https://go.nuxtjs.dev/config-modules)
+	modules: [
+		// https://go.nuxtjs.dev/axios
+		'@nuxtjs/axios',
+		'@nuxtjs/style-resources',
+		// https://go.nuxtjs.dev/pwa
+		'@nuxtjs/pwa',
+	],
 
-  // Axios module configuration (https://go.nuxtjs.dev/config-axios)
-  axios: {},
+	styleResources: {
+		sass: ['@/assets/_variables.sass', '@/assets/_utility.sass'],
+	},
 
-  // Build Configuration (https://go.nuxtjs.dev/config-build)
-  build: {
-  }
+	pwa: {
+		manifest: {
+			name: 'Conzepta',
+			short_name: 'Conzepta',
+			orientation: 'portrait',
+		},
+		// workbox: { dev: true },
+	},
+
+	// server: {
+	// 	host: '0.0.0.0',
+	// 	port: 3000,
+	// },
+
+	// Axios module configuration (https://go.nuxtjs.dev/config-axios)
+	axios: {},
+
+	// Build Configuration (https://go.nuxtjs.dev/config-build)
+	build: {},
 }
