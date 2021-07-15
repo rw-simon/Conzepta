cc3ae86ad5e2c5496bd09ddee5213798416670a8
rw-simon <simon.fischer@rechtwinklig.ch>
2021-07-15 14:24:32 +0200

    changed base url

--- nuxt.config.js
+++ nuxt.config.js
@@ -109,7 +109,7 @@ export default {
 	// },
 
 	// Axios module configuration (https://go.nuxtjs.dev/config-axios)
-	axios: {},
+	axios: { baseURL: 'https://www.conzepta.ch' },
 
 	// Build Configuration (https://go.nuxtjs.dev/config-build)
 	build: {},
